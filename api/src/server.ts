5e9bcfd7ebcd521e82517660e0b113433e51650c
joaoev <joaoalvesxp@alu.ufc.br>
2026-03-25 21:12:43 -0300

    init project

---
+++ api/src/server.ts
@@ -0,0 +1,13 @@
+import { fastify } from "fastify";
+
+const app = fastify();
+
+app
+	.listen({ port: 3333, host: "0.0.0.0" })
+	.then(() => {
+		console.log("Server is running on http://localhost:3333");
+	})
+	.catch((err) => {
+		console.error("Error starting server:", err);
+		process.exit(1);
+	});
